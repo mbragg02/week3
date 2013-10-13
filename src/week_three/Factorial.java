b817452fc7eb518d7c9cfbece6bca57ca017cb3f
Michael Bragg <mike.a.bragg@gmail.com>
2013-10-13 23:58:21 +0100

    Fibonacci Added

--- src/week_three/Factorial.java
+++ src/week_three/Factorial.java
@@ -3,11 +3,21 @@ package week_three;
 public class Factorial {
 
 	/**
-	 * @param args
+	 * The factorial of a natural number is represented with an exclamation mark (!) 
+	 * and defined as: 
+	 * n! = n x (n -1) x (n - 2) x . . . 2 x 1
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(factorial(4));
+	}
+	
+	public static int factorial (int n) {
+		if (n == 1) {
+			return 1;
+		} else {
+			int result = n * factorial(n - 1);
+			return result;
+		}
 	}
 
 }
---
+++ src/week_three/Fibonacci.java
@@ -0,0 +1,20 @@
+package week_three;
+
+public class Fibonacci {
+
+	/**
+	 * Fibonacci numbers
+	 */
+	public static void main(String[] args) {
+		System.out.println(fib(10));
+	}
+	public static int fib(int n) {
+		if ((n == 1) || (n == 2)) {
+	          return 1;
+	        } else {
+	        	int result = fib(n-1) + fib(n-2);
+	            return result;
+	        }
+	}
+
+}
